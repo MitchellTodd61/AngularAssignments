1ba55eead7a7ac481b10f3d80594de8cc8a07063
mitchellblain61 <mitchellblain@hotmail.com>
2020-10-05 10:59:30 -0400

    Commit #1
    Creating new files and folders for assignment. Interface skeleton built.

---
+++ assignment1/helper-files/content-interface.ts
@@ -0,0 +1,10 @@
+interface Content {
+  id: number;
+  author: string;
+  imgUrl: string; /*optional*/
+  type: string; /*optional*/
+  title: string;
+  body: string;
+  tags: [string]; /*optional*/
+
+}
