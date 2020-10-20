0d65425af7b91a10b6c071f42778c23489b69ba5
mitchellblain61 <mitchellblain@hotmail.com>
2020-10-20 17:17:37 -0400

    Assignment3
    Git error commit. Fixed

--- src/app/filter-type.pipe.ts
+++ src/app/filter-type.pipe.ts
@@ -1,9 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {Content} from './helper-files/content-interface';
-<<<<<<< HEAD
 import {ContentListComponent} from './content-list/content-list.component';
-=======
->>>>>>> 9fbb0820583a1a17ba49bf1991545c9b79544fb7
 
 @Pipe({
   name: 'filterType'
@@ -11,11 +8,7 @@ import {ContentListComponent} from './content-list/content-list.component';
 export class FilterTypePipe implements PipeTransform {
 
   transform(value: Content[], filterString: string): Content[] {
-<<<<<<< HEAD
     return value;
-=======
-    return null;
->>>>>>> 9fbb0820583a1a17ba49bf1991545c9b79544fb7
   }
 
 }
